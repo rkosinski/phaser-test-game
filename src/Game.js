8205f0f8693a29d6bafe9929b4376776638b5bee
Radoslaw Kosinski <r_kosinski@o2.pl>
2014-12-29 20:45:31 +0100

    update

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,35 @@
-Distro.Game = function (game) {};
+Distro.Game = function (game) {
+    rocks = null;
+    bigRocks = 2;
+    mediumRocks = 3;
+    smallRocks = 4;
+};
 
 Distro.Game.prototype = {
     create: function () {
+        // Background
+        this.add.sprite(0, 0, 'background');
+
+        // Create rocks group
+        this.rocks = this.add.group();
+
+        // Create big rocks
+        for (var i = 0; i < bigRocks; i++) {
+            this.rocks.create(Math.random() * 400, Math.random() * 540, 'rockBig');
+        }
+
+        // Create medium rocks
+        for (var i = 0; i < mediumRocks; i++) {
+            this.rocks.create(Math.random() * 400, Math.random() * 540, 'rockMedium');
+        }
+
+        // Create small rocks
+        for (var i = 0; i < smallRocks; i++) {
+            this.rocks.create(Math.random() * 400, Math.random() * 540, 'rockSmall');
+        }
+
         console.log('From game.js');
+    },
+    update: function() {
     }
 };
