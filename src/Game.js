e8ec94ac1f7b682d38d8b7fb45b3540d95dc3721
Radoslaw Kosinski <r_kosinski@o2.pl>
2014-12-30 22:28:37 +0100

    code refactoring

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,9 @@
 Distro.Game = function (game) {
-    counter = 0;
-    step = Math.PI * 2 / 360;
-    rocks = null;
-    stagesRocks = {
+    gravityCounter = 0;
+    gravitySteps = Math.PI * 2 / 360;
+    currentStage = 'firstStage';
+
+    rockItems = {
         'firstStage': {
             'rockBig': [
                 [50, 190], [280, 340]
@@ -12,6 +13,13 @@ Distro.Game = function (game) {
             ],
             'rockSmall': [
                 [340, 70], [65, 510], [200, 240], [330, 550]
+            ]
+        }
+    }
+    crystalItems = {
+        'firstStage': {
+            'crystals': [
+                [260, 190, 1], [100, 330, 2], [360, 470, 0]
             ]
         }
     }
@@ -25,37 +33,53 @@ Distro.Game.prototype = {
         this.physics.startSystem(Phaser.Physics.ARCADE);
         // Create rocks group
         this.rocks = this.add.group();
+        // Create crystals group
+        this.crystals = this.add.group();
+        // Set stage items groups (rocks and crystals)
+        this.setStageItemsGroups(rockItems, this.rocks);
+        this.setStageItemsGroups(crystalItems, this.crystals);
 
-        // Add rocks to the group
-        for (var key in stagesRocks.firstStage) {
-            if (key.match(/(rock){1}[a-zA-Z]*/)) {
-                for (var i = 0; i < stagesRocks.firstStage[key].length; i++) {
-                    this.rocks.create(
-                        stagesRocks.firstStage[key][i][0], 
-                        stagesRocks.firstStage[key][i][1],
-                        key
-                    );
-                }
+        this.setRocksAnchorPointsAndEnableGravity();
+        this.setCrystalsRandomAngleAndEnableGravity();
+    },
+    setStageItemsGroups: function (items, itemGroup) {
+        for (var key in items[currentStage]) {
+            for (var i = 0; i < items[currentStage][key].length; i++) {
+                itemGroup.create(
+                    items[currentStage][key][i][0], 
+                    items[currentStage][key][i][1],
+                    key,
+                    items[currentStage][key][i][2]
+                );
             }
         }
-
-        // Set anchor point to every rock in stage
-        // And enable gravity on it
+    },
+    setRocksAnchorPointsAndEnableGravity: function () {
         for (var key in this.rocks.children) {
             this.rocks.children[key].anchor.set(0.5);
             this.physics.arcade.enable(this.rocks.children[key]);
         }
     },
+    setCrystalsRandomAngleAndEnableGravity: function () {
+        for (var key in this.crystals.children) {
+            this.crystals.children[key].angle = Math.floor(Math.random() * (360 - 0 + 1) + 0);
+            this.physics.arcade.enable(this.crystals.children[key]);
+        }
+    },
     update: function () {
-        var tStep = Math.sin(counter);
+        this.setItemsGravityBounceAndRotation(this.rocks, 2, true);
+        this.setItemsGravityBounceAndRotation(this.crystals, 5, false);
+    },
+    setItemsGravityBounceAndRotation: function (itemGroup, bounceDistance, isRotation) {
+        var tStep = Math.sin(gravityCounter);
 
-        // Gravity bounce rocks
-        // And rotate them
-        for (var key in this.rocks.children) {
-            this.rocks.children[key].body.y = this.rocks.children[key].body.y + tStep / 2;
-            this.rocks.children[key].angle += 0.2;
+        for (var key in itemGroup.children) {
+            itemGroup.children[key].body.y = itemGroup.children[key].body.y + tStep / bounceDistance;
+            if (isRotation) {
+                itemGroup.children[key].angle += 0.2;
+            }
         }
 
-        counter += step;
-    },
+        gravityCounter += gravitySteps;
+    }
 };
